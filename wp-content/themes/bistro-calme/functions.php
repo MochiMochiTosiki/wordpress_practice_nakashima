da0e32eb2c4dd30d18e83f984ba7ebd0512c3355
MochimochiToshiki <mochizuki@haw.co.jp>
2021-03-16 18:42:51 +0900

    section05 分割したテンプレートを作り込む

---
+++ wp-content/themes/bistro-calme/functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * <title>タグを出力する
+ */
+add_theme_support( 'title-tag' );
+
+add_filter( 'document_title_separator', 'my_document_title_separator' );
+function my_document_title_separator($separator){
+    $separator = '|';
+    return $separator;
+}
+
+add_filter( 'document_title_parts', 'my_document_title_parts' );
+function my_document_title_parts($title){
+    if (is_home()) {
+        unset($title['tagline']);
+        $title['title'] ='BISTRO CALMEはカジュアルなワインバーよりビストロです。';
+        //テキストを変更
+    }
+    return $title;
+}
