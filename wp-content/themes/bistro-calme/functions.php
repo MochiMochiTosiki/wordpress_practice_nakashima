dc6181f41c7f5b73b473dfb0c0cabdbf2d1ee488
MochimochiToshiki <mochizuki@haw.co.jp>
2021-03-16 19:42:15 +0900

    section06 WordPressループを作成する

--- wp-content/themes/bistro-calme/functions.php
+++ wp-content/themes/bistro-calme/functions.php
@@ -4,18 +4,28 @@
  */
 add_theme_support( 'title-tag' );
 
+/**
+ * タイトルタグの区切り文字をエン・ダッシュから縦線に変更する
+ */
 add_filter( 'document_title_separator', 'my_document_title_separator' );
 function my_document_title_separator($separator){
     $separator = '|';
     return $separator;
 }
 
+/**
+ * タイトルのタグのテキストを変更する
+ */
 add_filter( 'document_title_parts', 'my_document_title_parts' );
 function my_document_title_parts($title){
     if (is_home()) {
-        unset($title['tagline']);
-        $title['title'] ='BISTRO CALMEはカジュアルなワインバーよりビストロです。';
-        //テキストを変更
+        unset($title['tagline']); // タグラインを削除
+        $title['title'] ='BISTRO CALMEはカジュアルなワインバーよりビストロです。'; //テキストを変更
     }
     return $title;
 }
+
+/**
+ * アイキャッチ画像を使用可能にする
+ */
+add_theme_support( 'post-thumbnails' );
