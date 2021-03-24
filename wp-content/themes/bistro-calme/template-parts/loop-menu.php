c31050a92a4c68623bf06b0f52b901e158d81a20
MochimochiToshiki <mochizuki@haw.co.jp>
2021-03-24 14:50:04 +0900

    section01 archive-menu.php とloop-menu.php を追加

---
+++ wp-content/themes/bistro-calme/template-parts/loop-menu.php
@@ -0,0 +1,14 @@
+<section class="menu">
+    <a href="#">
+        <figure class="menu_pic">
+            <img src="./assets/img/menu/menu-10@2x.jpg" alt="">
+        </figure>
+        <h3 class="menu_title">メニュー名が入ります</h3>
+        <p class="menu_price">800円</p>
+        <div class="menu_desc">
+            <p>
+                概要が入ります。概要が入ります。概要が入ります。概要が入ります。
+            </p>
+        </div>
+    </a>
+</section>
